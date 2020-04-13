d257722154d3c46d0cf00ab411b81b85de091492
Stanislav Shpitalnik <stanislavshpitalnik@Stanislavs-MacBook-Pro.local>
2020-04-13 05:46:38 -0700

    Enable admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -7,43 +10,36 @@ class UserProfileManager(BaseUserManager):
 
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
-        if not email: 
+        if not email:
             raise ValueError('User must have an email address')
-        
-        # letter case second half of email address
+
         email = self.normalize_email(email)
-        # creates a user model 
-        # and sets the email and the name 
         user = self.model(email=email, name=name)
 
-        # part of the AbstractBaseUser
-        # Reason for this is to converted to a hash 
-        # hacker would only be able to see the hashed password
         user.set_password(password)
         user.save(using=self._db)
 
-        # return newly create user object
         return user
 
-        def create_superuser(self, email, name, password):
-            """Create and save a new superuser wuth given details"""
-            user = self.create_user(email, name, password )
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            # create by the PermissionsMixin
-            user.is_superuser = True
-            user.is_staff = True 
-            user.save(using=self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            return user
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True) # if a user's profile is active
-    is_staff = models.BooleanField(default=False) # if user is staff = Admin
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    objects = UserProfileManager() #going to create class later
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
@@ -53,9 +49,71 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.name
 
     def get_short_name(self):
-        """Retieve short name of user"""
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+
+
+
+
+# class UserProfileManager(BaseUserManager):
+#     """Manager for user profiles"""
+
+#     def create_user(self, email, name, password=None):
+#         """Create a new user profile"""
+#         if not email: 
+#             raise ValueError('User must have an email address')
+        
+#         # letter case second half of email address
+#             email = self.normalize_email(email)
+#         # creates a user model 
+#         # and sets the email and the name 
+#             user = self.model(email=email, name=name)
+
+#         # part of the AbstractBaseUser
+#         # Reason for this is to converted to a hash 
+#         # hacker would only be able to see the hashed password
+#             user.set_password(password)
+#             user.save(using=self._db)
+
+#         # return newly create user object
+#         return user
+
+#         def create_superuser(self, email, name, password):
+#             """Create and save a new superuser wuth given details"""
+#             user = self.create_user(email, name, password )
+
+#             # create by the PermissionsMixin
+#             user.is_superuser = True
+#             user.is_staff = True 
+#             user.save(using=self._db)
+
+#             return user
+
+# class UserProfile(AbstractBaseUser, PermissionsMixin):
+#     """Database model for users in the system"""
+#     email = models.EmailField(max_length=255, unique=True)
+#     name = models.CharField(max_length=255)
+#     is_active = models.BooleanField(default=True) # if a user's profile is active
+#     is_staff = models.BooleanField(default=False) # if user is staff = Admin
+
+#     objects = UserProfileManager() #going to create class later
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['name']
+
+#     def get_full_name(self):
+#         """Retrieve full name of user"""
+#         return self.name
+
+#     def get_short_name(self):
+#         """Retieve short name of user"""
+#         return self.name
+
+#     def __str__(self):
+#         """Return string representation of our user"""
+#         return self.email
