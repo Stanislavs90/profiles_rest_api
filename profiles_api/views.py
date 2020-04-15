eb6d8ca8ba13f88f044507ad44fc02613596cfdb
Stanislavs90 <stanislavshpitalnik@Stanislavs-MacBook-Pro.local>
2020-04-15 12:03:39 -0700

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status # list of HTTP status codes when returning respones 
 from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication # makes a random token string
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -117,3 +119,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 #     queryset = models.UserProfile.objects.all() # funtions: create , list , update, partial update , destory
 #     authentication_classes = (TokenAuthentication,) # how user will authentication
 #     permission_classes = (permissions.UpdateOwnProfile,) # permission to use different apis 
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
