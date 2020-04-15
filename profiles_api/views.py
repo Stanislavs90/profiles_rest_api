07d3968a26c92a666afb21f1fe62fcb96c7eba7b
Stanislavs90 <stanislavshpitalnik@Stanislavs-MacBook-Pro.local>
2020-04-15 02:27:14 -0700

    to fix error

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status # list of HTTP status codes when returning respones 
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication # makes a random token string
 
 from profiles_api import serializers
@@ -107,6 +107,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email', )
 
 
 # class UserProfileViewSet(viewsets.ModelViewSet):
