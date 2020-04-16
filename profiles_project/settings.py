94750d75f4e98ff2fb810d15954ea49751cb60da
Stanislavs90 <stanislavshpitalnik@Stanislavs-MacBook-Pro.local>
2020-04-15 18:06:36 -0700

    Commited for deployment
    `#

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z3xft9j^vc+k9mlztr4b+6=t^%p2%q00&9&y%4)#r&v#^mwgzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
